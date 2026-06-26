3323d49567cb820e2d7e749baaa63f917d20d50c
roshhellwett <roshhellwett@icloud.com>
2026-06-26 23:16:21 +0530

    zenithpages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/zenithpages/',
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [react(), tailwindcss()],
 })
